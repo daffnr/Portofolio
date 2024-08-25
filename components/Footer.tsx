f7d0021c92c8e27c5a3eae6029a42608edaeacba
daffnr <daffanr20@gmail.com>
2024-08-25 18:05:57 +0700

    update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ import {
 } from "@heroicons/react/20/solid";
 import React from "react";
 
-const Footer = ({id}: any) => {
+const Footer = ({ id }: any) => {
   return (
     <div id={id} className="pt-[8rem] pb-[4rem] bg-[#02050a]">
       <div className="grid border-b-[1px] pb-[6rem] border-gray-400 grid-cols-1 lg:grid-cols-3 md:grid-cols-2 w-[80%] mx-auto gap-[3rem]">
@@ -18,7 +18,7 @@ const Footer = ({id}: any) => {
               Address
             </h1>
             <p className="text-[17px] w-[90%] text-white opacity-60">
-            Bekasi, Permata legenda Blok K8 / 1. 17156
+              Bekasi, Permata legenda Blok K8 / 1. 17156
             </p>
           </div>
         </div>
@@ -32,6 +32,13 @@ const Footer = ({id}: any) => {
             </h1>
             <p className="text-[17px] w-[90%] text-white opacity-60">
               +6287785349292
+              <br/>
+              <a
+                href="https://wa.link/8wib7w"
+                className="text-green-400 underline ml-[0.2rem] whitespace-nowrap"
+              >
+               Click to WhatsApp
+              </a>
             </p>
           </div>
         </div>
@@ -41,7 +48,7 @@ const Footer = ({id}: any) => {
           </div>
           <div>
             <h1 className="text-[25px] mb-[0.2rem] font-semibold text-[white]">
-              Send Us Email
+              Send Email
             </h1>
             <p className="text-[17px] w-[90%] text-white opacity-60">
               daffanr20@gmail.com
@@ -51,12 +58,12 @@ const Footer = ({id}: any) => {
       </div>
       <div className="w-[80%] mt-[2rem] mx-auto grid grid-cols-1 md:grid-cols-2 items-center justify-between">
         <div className="text-[18px] mb-[2rem] md:mb-0 text-white opacity-20">
-            WebDev 2024 | All Rights Reserved
+          WebDev 2024 | All Rights Reserved
         </div>
         <div className="flex items-center space-x-10">
-            <p className="text-[16px] text-white opacity-20">Terms & Condition</p>
-            <p className="text-[16px] text-white opacity-20">Privacy Policy</p>
-            <p className="text-[16px] text-white opacity-20">Sitemap</p>
+          <p className="text-[16px] text-white opacity-20">Terms & Condition</p>
+          <p className="text-[16px] text-white opacity-20">Privacy Policy</p>
+          <p className="text-[16px] text-white opacity-20">Sitemap</p>
         </div>
       </div>
     </div>
