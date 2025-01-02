a71406d20d821198dd684420eafda790a6329b85
daffnr <daffanr20@gmail.com>
2025-01-03 05:44:54 +0700

    update project love quotes

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 import React from "react";
-import router from 'next/router'
-    
-const Projects = ({id}:any) => {
+import router from "next/router";
+
+const Projects = ({ id }: any) => {
   return (
     <div id={id} className="bg-[#02050a] pt-[4rem] md:pt-[8rem] pb-[1rem]">
       <h1 className="heading">
@@ -10,7 +10,12 @@ const Projects = ({id}:any) => {
       </h1>
       <div className="w-[80%] pt-[2rem] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[2rem]">
         <div>
-          <div onClick={() => router.push("https://web-company-profile-three.vercel.app/home")} className="transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]">
+          <div
+            onClick={() =>
+              router.push("https://web-company-profile-three.vercel.app/home")
+            }
+            className="transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]"
+          >
             <Image
               src="/images/companyWeb.png"
               alt="portofolio"
@@ -20,7 +25,12 @@ const Projects = ({id}:any) => {
           </div>
         </div>
         <div>
-          <div onClick={() => router.push ("https://employee-list-liard.vercel.app/")} className="transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]">
+          <div
+            onClick={() =>
+              router.push("https://employee-list-liard.vercel.app/")
+            }
+            className="transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]"
+          >
             <Image
               src="/images/employeeList.png"
               alt="portofolio"
@@ -30,9 +40,14 @@ const Projects = ({id}:any) => {
           </div>
         </div>
         <div>
-          <div className="transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]">
+          <div
+            onClick={() =>
+              router.push("https://random-quotes-generator-puce.vercel.app/")
+            }
+            className="transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]"
+          >
             <Image
-              src="/images/project.png"
+              src="/images/quotesGenerator.png"
               alt="portofolio"
               layout="fill"
               className="object-contain"
