faf24a0b49b8b7b5b99f5207af9b4589b71031b6
daffnr <daffanr20@gmail.com>
2025-01-10 04:03:58 +0700

    update project contact app

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -54,6 +54,21 @@ const Projects = ({ id }: any) => {
             />
           </div>
         </div>
+        <div>
+          <div
+            onClick={() =>
+              router.push("https://my-contact-app-kappa.vercel.app/")
+            }
+            className="transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]"
+          >
+            <Image
+              src="/images/my-contact-app.png"
+              alt="portofolio"
+              layout="fill"
+              className="object-contain"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
