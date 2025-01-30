f839fc7d8fa8b1a8de5ef0d511a3da6462761d28
daffnr <daffanr20@gmail.com>
2025-01-30 15:15:42 +0700

    post breakingBad

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -69,6 +69,21 @@ const Projects = ({ id }: any) => {
             />
           </div>
         </div>
+        <div>
+          <div
+            onClick={() =>
+              router.push("https://breaking-bad-characters-rose.vercel.app/")
+            }
+            className="transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]"
+          >
+            <Image
+              src="/images/breakingBad.png"
+              alt="portofolio"
+              layout="fill"
+              className="object-contain"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
