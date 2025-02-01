be409f0477551fcc5cf15122b39073d51b33fcd6
daffnr <daffanr20@gmail.com>
2025-02-01 21:11:58 +0700

    update bio

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -58,7 +58,7 @@ const Footer = ({ id }: any) => {
       </div>
       <div className="w-[80%] mt-[2rem] mx-auto grid grid-cols-1 md:grid-cols-2 items-center justify-between">
         <div className="text-[18px] mb-[2rem] md:mb-0 text-white opacity-20">
-          WebDev 2024 | All Rights Reserved
+          DNRWeb 2024 | All Rights Reserved
         </div>
         <div className="flex items-center space-x-10">
           <p className="text-[16px] text-white opacity-20">Terms & Condition</p>
